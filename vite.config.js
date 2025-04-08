1e3ce403f0527ca1b75cf3d0e5ea31b5bf2a0062
math1207 <mathias.goldmann@yahoo.fr>
2025-04-08 12:06:55 +0300

    chore: reset configuration for new vercel project

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'pdf-lib': ['pdf-lib'],
-          'ui-vendor': ['@chakra-ui/react', '@emotion/react', '@emotion/styled', 'framer-motion']
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000
-  },
   server: {
     port: 3000
   }
